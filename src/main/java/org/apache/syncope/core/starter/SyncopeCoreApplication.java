8c0eb4c2307d94b93c2cd98ca235a75ecbeba52c
Francesco Chicchiriccò <ilgrosso@apache.org>
2022-03-13 14:50:49 +0100

    Initial commit

---
+++ src/main/java/org/apache/syncope/core/starter/SyncopeCoreApplication.java
@@ -0,0 +1,12 @@
+package org.apache.syncope.core.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncopeCoreApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SyncopeCoreApplication.class, args);
+    }
+}
